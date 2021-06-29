7f9c229d7dc55bc743e6330c919b9e53fe4ffe89
kiethoang214 <48423316+kiethoang214@users.noreply.github.com>
2021-06-29 16:07:36 +0700

    decode url from client

--- WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -10,90 +10,72 @@ using System.Web;
 
 namespace TestAPI2.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
-    {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+	[ApiController]
+	[Route("[controller]")]
+	public class WeatherForecastController : ControllerBase
+	{
+		private static readonly string[] Summaries = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
 
-        private readonly ILogger<WeatherForecastController> _logger;
+		private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+		public WeatherForecastController(ILogger<WeatherForecastController> logger)
+		{
+			_logger = logger;
+		}
 
-        [HttpGet("getWebContent/{link}")]
-        public String getWebContent(String link)
-        {
-            try {
-                string newlink = HttpUtility.UrlDecode(link);
-                return getWeb(newlink);
-            } catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return e.Message;
-            }
-        }
-
-        class HtmlData
-        {
-            public String title { get; set; }
-            public String description { get; set; }
-            public Uri images { get; set; }
-            public Uri url { get; set; }
-        }
-
-        protected string getWeb(String url)
-        {
-            HtmlData data = new HtmlData();
-            try
-            {
-                var getHtmlDoc = new HtmlWeb();
-                var document = getHtmlDoc.Load(url);
-                var metaTags = document.DocumentNode.SelectNodes("//meta");
-                if (metaTags != null)
-                {
-                    foreach (var sitetag in metaTags)
-                    {
-                        if (sitetag.Attributes["property"] != null && sitetag.Attributes["property"].Value == "og:title")
-                        {
-                            data.title = sitetag.Attributes["content"].Value;
-                        }
-
-                        if (sitetag.Attributes["property"] != null && sitetag.Attributes["property"].Value == "og:description")
-                        {
-                            data.description = sitetag.Attributes["content"].Value;
-                        }
-
-                        if (sitetag.Attributes["property"] != null && sitetag.Attributes["property"].Value == "og:image")
-                        {
-                            data.images = new Uri(sitetag.Attributes["content"].Value, UriKind.Absolute);
-                        }
-
-                        if (sitetag.Attributes["property"] != null && sitetag.Attributes["property"].Value == "og:url")
-                        {
-                            data.url = new Uri(sitetag.Attributes["content"].Value, UriKind.Absolute);
-                        }
-                    }
-
-                    if (data.url == null)
-                    {
-                        data.url = new Uri(url);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return e.Message;
-            }
-            
-            string stringjson = JsonConvert.SerializeObject(data);
-            return stringjson;
-        }
-    }
+		[HttpGet("getWebContent/{link}")]
+		public String getWebContent(String link)
+		{
+			string newlink = HttpUtility.UrlDecode(link);
+			WebInfo data = new WebInfo();
+			try
+			{
+				var getHtmlDoc = new HtmlWeb();
+				var document = getHtmlDoc.Load(newlink);
+				var metaTags = document.DocumentNode.SelectNodes("//meta");
+				if (metaTags != null)
+				{
+					foreach (var sitetag in metaTags)
+					{
+						if (sitetag.Attributes["property"] != null && sitetag.Attributes["property"].Value == "og:title")
+						{
+							data.title = sitetag.Attributes["content"].Value;
+						}
+						if (sitetag.Attributes["property"] != null && sitetag.Attributes["property"].Value == "og:description")
+						{
+							data.description = sitetag.Attributes["content"].Value;
+						}
+						if (sitetag.Attributes["property"] != null && sitetag.Attributes["property"].Value == "og:image")
+						{
+							data.images = new Uri(sitetag.Attributes["content"].Value, UriKind.Absolute);
+						}
+						if (sitetag.Attributes["property"] != null && sitetag.Attributes["property"].Value == "og:url")
+						{
+							data.url = new Uri(sitetag.Attributes["content"].Value, UriKind.Absolute);
+						}
+					}
+					if (data.url == null)
+					{
+						data.url = new Uri(link);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				return "";
+			}
+			string stringjson = JsonConvert.SerializeObject(data);
+			return stringjson;
+		}
+		class WebInfo
+		{
+			public String title { get; set; }
+			public String description { get; set; }
+			public Uri images { get; set; }
+			public Uri url { get; set; }
+		}
+	}
 }
